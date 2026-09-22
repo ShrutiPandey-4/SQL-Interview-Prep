59f4f8f60815d11963b9718b3ebd0df3c13f4648
ShrutiPandey-4 <23053394@kiit.ac.in>
2026-09-23 01:26:06 +0530

    Added few questions

---
+++ 01-Basics/Level-4-GROUPBY_HAVING.sql
@@ -0,0 +1,31 @@
+-- Basic difference:
+-- WHERE → individual rows ko filter karta hai before grouping
+-- HAVING → groups ko filter karta hai after GROUP BY
+
+-- Q29
+
+-- Find number of customers in each state.
+
+-- Q30
+
+-- Find number of orders for each status.
+
+-- Q31
+
+-- Find sellers who have sold more than 100 items.
+
+-- Q32
+
+-- Find customers who placed more than 3 orders.
+
+-- Q33
+
+-- Find product categories with average price > 100.
+
+-- Q34
+
+-- Find states having more than 1,000 orders.
+
+-- Q35
+
+-- Find sellers whose total sales exceed 10,000.
